0191def97aabac7ce5cc7cfdc77642d85bed99e1
trg <cactusx09@gmail.com>
2020-08-26 23:47:15 +0300

    PORT env

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 
 const dotenv = require('dotenv')
 dotenv.config()
-const port = process.env.API_PORT || 8080
+const port = process.env.PORT || 8080
 
 const cors = require('cors')
 app.use(cors())
