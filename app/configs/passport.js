5436c5d4f25afbaf799deae5886cc886b405c99e
trg <cactusx09@gmail.com>
2020-08-28 13:06:14 +0300

    forms validation and error handling

--- app/configs/passport.js
+++ app/configs/passport.js
@@ -11,23 +11,25 @@ passport.use(
       passwordField: 'password',
     },
     (login, password, done) => {
-      db.get(
-        'SELECT * FROM user WHERE login = ?',
-        [login],
-        async (err, row) => {
-          if (err) {
-            return done(err)
-          }
-          if (!row) {
-            return done(null, false, { error: 'Invalid login' })
-          }
-          const isPasswordMatch = await bcrypt.compare(password, row.password)
-          if (!isPasswordMatch) {
-            return done(null, false, { error: 'invalid password' })
-          }
-          done(null, row)
+      db.get('SELECT * FROM user WHERE login = ?', login, async (err, row) => {
+        if (err) {
+          return done(err)
         }
-      )
+        if (!row) {
+          return done(null, false, {
+            message: 'This username not found',
+            field: 'login',
+          })
+        }
+        const isPasswordMatch = await bcrypt.compare(password, row.password)
+        if (!isPasswordMatch) {
+          return done(null, false, {
+            message: 'This password do not match',
+            field: 'password',
+          })
+        }
+        done(null, row)
+      })
     }
   )
 )
--- pages/login.vue
+++ pages/login.vue
@@ -1,37 +1,50 @@
 <template lang="pug">
-  div.section
+  div.section.login
     .container
       .columns
         .column.is-one-third.is-offset-one-third
-          //- .error {{ error }}
           h1.title.has-text-centered Login
           form(@submit.prevent="userLogin")
-            .field.my-5
+            .field.login__username.mb-5
               label.label Username
               .control.has-icons-left.has-icons-right
                 input.input(
-                  v-model="login.login"
+                  :class="{ 'is-danger': login.error }"
+                  v-model="login.value"
                   type='text'
                 )
                 span.icon.is-small.is-left
                   v-icon(name="user")
-                //- span.icon.is-small.is-right
-              //- p.help.is-success This username is available
-            .field
+                template(v-if="login.error")
+                  p.help.is-danger {{ login.error }}!
+                    nuxt-link.ml-1(to="signup") Signup
+                  a.icon.is-small.is-right.has-text-danger(
+                    @click.prevent="login.value = ''; login.error = null"
+                  )
+                    v-icon(name="times")
+            .field.login__password
               label.label Password
               .control.has-icons-left.has-icons-right
                 input.input(
-                  v-model="login.password"
+                  :class="{ 'is-danger': password.error }"
+                  v-model="password.value"
                   type="password"
                 )
                 span.icon.is-small.is-left
                   v-icon(name="envelope")
-                //- span.icon.is-small.is-right
-              //- p.help.is-danger This email is invalid
-
+                span.icon.is-small.is-right
+                template(v-if="password.error")
+                  p.help.is-danger {{ password.error }}
+                  a.icon.is-small.is-right.has-text-danger(
+                    @click.prevent="password.value = ''; password.error = ''"
+                  )
+                    v-icon(name="times")
             .columns.pt-4
               .column
-                button.button.is-medium.is-success.is-fullwidth(type="submit") Submit
+                button.button.is-medium.is-success.is-fullwidth(
+                  :disabled="login.value.length === 0 || password.value.length === 0"
+                  type="submit"
+                ) Submit
 </template>
 
 <script>
@@ -39,24 +52,45 @@ export default {
   data() {
     return {
       login: {
-        login: '',
-        password: '',
+        value: '',
+        error: null,
       },
-      error: null,
+      password: {
+        value: '',
+        error: null,
+      },
     }
   },
   methods: {
     async userLogin() {
-      try {
-        const response = await this.$auth.loginWith('local', {
-          data: this.login,
+      await this.$auth
+        .loginWith('local', {
+          data: {
+            login: this.login.value,
+            password: this.password.value,
+          },
         })
-        console.log(response)
-      } catch (err) {
-        console.log(err)
-        this.error = err
-      }
+        .then(() => {
+          console.log('Successully login')
+        })
+        .catch(({ response }) => {
+          if (response.status === 401) {
+            this.login.error = null
+            this.password.error = null
+
+            const { field, message } = response.data
+            this[field].error = message
+          }
+        })
     },
   },
 }
 </script>
+
+<style lang="sass" scoped>
+.login
+  &__username,
+  &__password
+    .icon.is-small.is-right
+      pointer-events: all
+</style>
